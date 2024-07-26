031be35525b76600468ead68234dc348381f7198
rahul kumar <142343501+CodeSciRahul@users.noreply.github.com>
2024-07-26 22:30:40 +0530

    reset: clear main branch

--- Backend/src/routes/ProductRoute.js
+++
@@ -1,8 +0,0 @@
-import Router from "express"
-import { getAllProducts } from "../controllers/ProductController"
-
-const router  = Router();
-
-router.get("/",getAllProducts)
-
-export default router
